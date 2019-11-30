c1cdef212c52b682691117ee1de709e8e6d8c3bc
Timothée <td-8@hotmail.fr>
2019-11-30 16:27:06 +0100

    Main menu view + others activities set up

---
+++ app/src/main/java/fr/utt/if26/projetif26_drouotrenard/Utiliser/UtiliserActivity.java
@@ -0,0 +1,16 @@
+package fr.utt.if26.projetif26_drouotrenard.Utiliser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import fr.utt.if26.projetif26_drouotrenard.R;
+
+public class UtiliserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_utiliser);
+    }
+}
