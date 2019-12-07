20dbe74e7b602cbc64a6a3f604c4d06af9349bd3
Timothée <td-8@hotmail.fr>
2019-12-07 19:36:20 +0100

    Added all activities and some views

--- app/src/main/java/fr/utt/if26/projetif26_drouotrenard/Parametrer/Parametrer2Activity.java
+++ app/src/main/java/fr/utt/if26/projetif26_drouotrenard/Parametrer/Parametrer2Activity.java
@@ -1,4 +1,4 @@
-package fr.utt.if26.projetif26_drouotrenard.Tracabilite;
+package fr.utt.if26.projetif26_drouotrenard.Parametrer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,22 +9,22 @@ import android.widget.Button;
 
 import fr.utt.if26.projetif26_drouotrenard.R;
 
-public class TracabiliteActivity extends AppCompatActivity implements View.OnClickListener {
+public class Parametrer2Activity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tracabilite);
+        setContentView(R.layout.activity_parametrer2);
 
-        Button btn = (Button) findViewById(R.id.tracabiliteView_creer);
+        Button btn = (Button) findViewById(R.id.parametrer2View_btn);
         btn.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.tracabiliteView_creer:
-                Intent intent = new Intent(this, Tracabilite2Activity.class);
+            case R.id.parametrer2View_btn:
+                Intent intent = new Intent(this, Parametrer3Activity.class);
                 startActivity(intent);
                 break;
             default:
--- app/src/main/java/fr/utt/if26/projetif26_drouotrenard/Parametrer/ParametrerActivity.java
+++ app/src/main/java/fr/utt/if26/projetif26_drouotrenard/Parametrer/ParametrerActivity.java
@@ -2,15 +2,33 @@ package fr.utt.if26.projetif26_drouotrenard.Parametrer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import fr.utt.if26.projetif26_drouotrenard.R;
 
-public class ParametrerActivity extends AppCompatActivity {
+public class ParametrerActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parametrer);
+
+        Button btn = (Button) findViewById(R.id.parametrerView_button);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.parametrerView_button:
+                Intent intent = new Intent(this, Parametrer2Activity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
